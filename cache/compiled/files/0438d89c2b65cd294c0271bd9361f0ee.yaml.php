abdae473ec08e330a6b1257e1094bcd16c4268ad
Vincent Zhang <vzhang@opentable.com>
2016-04-07 23:09:10 -0700

    Added git ignore, removed logs and cache

--- cache/compiled/files/0438d89c2b65cd294c0271bd9361f0ee.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/vincent/Documents/Code/wordpress-galaxy/taiga_dev/user/config/streams.yaml',
-    'modified' => 1457221107,
-    'data' => [
-        
-    ]
-];
--- cache/compiled/files/8021390a7bf5854226685dcbe7394e69.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/vincent/Documents/Code/wordpress-galaxy/taiga_dev/user/config/media.yaml',
-    'modified' => 1457221107,
-    'data' => [
-        
-    ]
-];
