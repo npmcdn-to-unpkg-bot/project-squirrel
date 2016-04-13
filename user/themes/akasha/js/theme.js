eb9d10e263411debab02efff1e38bd5d71e22cb0
Vincent Zhang <vzhang@opentable.com>
2016-04-12 20:07:48 -0700

    Added mixin

--- user/themes/akasha/js/theme.js
+++ user/themes/akasha/js/theme.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($){
   console.log('jQuery version', jQuery.fn.jquery);
 
-  var $galleryHome = $('.gallery--home').imagesLoaded(function() {
-    $galleryHome.masonry({
-      gutter: 2,
-      percentPosition: true,
-      itemSelector: '.gallery--home__img'
-    });
-  });
+  // var $galleryHome = $('.gallery--home').imagesLoaded(function() {
+  //   $galleryHome.masonry({
+  //     gutter: 2,
+  //     percentPosition: true,
+  //     itemSelector: '.gallery--home__img'
+  //   });
+  // });
 });
