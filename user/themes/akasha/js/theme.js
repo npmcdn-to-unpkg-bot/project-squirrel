9890468ef878dee352d862ac1280733b94396a08
Vincent Zhang <vzhang@opentable.com>
2016-05-24 10:10:56 -0700

    Added scroller markup to cosplay section

--- user/themes/akasha/js/theme.js
+++ user/themes/akasha/js/theme.js
@@ -1,11 +1,4 @@
 jQuery(document).ready(function($){
   console.log('jQuery version', jQuery.fn.jquery);
 
-  // var $galleryHome = $('.gallery--home').imagesLoaded(function() {
-  //   $galleryHome.masonry({
-  //     gutter: 2,
-  //     percentPosition: true,
-  //     itemSelector: '.gallery--home__img'
-  //   });
-  // });
 });
